8582d81fa6f8f7e2ccd10e0e8b0e6e7696f46b07
吴振溢 <372620060@qq.com>
2023-03-12 16:23:55 +0800

    update

--- src/model.py
+++ src/model.py
@@ -3,6 +3,56 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+# 通道注意力机制
+class ChannelAttention(nn.Module):
+    def __init__(self, in_planes, ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+
+        self.fc1   = nn.Conv2d(in_planes, in_planes // 16, 1, bias=False)
+        self.relu1 = nn.ReLU()
+        self.fc2   = nn.Conv2d(in_planes // 16, in_planes, 1, bias=False)
+
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
+        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
+        out = avg_out + max_out
+        return self.sigmoid(out)
+
+
+# 空间注意力机制
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return self.sigmoid(x)
+
+# CBAM注意力模块
+class CBAM(nn.Module):
+    def __init__(self, c1, c2):
+        super(CBAM, self).__init__()
+        self.channel_attention = ChannelAttention(c1)
+        self.spatial_attention = SpatialAttention()
+
+    def forward(self, x):
+        out = self.channel_attention(x) * x
+        out = self.spatial_attention(out) * out
+        return out
+
 class Attn(nn.Module):
     def __init__(self, channel, reduction=16):
         super(Attn, self).__init__()
@@ -177,7 +227,8 @@ class U2Net(nn.Module):
         #最后将6层分割图通过1*1卷积融合成最终结果
         #我将要在这里做注意力机制
         #self.out_conv = nn.Conv2d(self.encode_num * out_ch, out_ch, kernel_size=1)
-        self.out_conv = RefineModule(self.encode_num * out_ch, out_ch)
+        #self.out_conv = RefineModule(self.encode_num * out_ch, out_ch)
+        self.out_conv = CBAM(self.encode_num * out_ch, out_ch)
 
     def forward(self, x: torch.Tensor) -> Union[torch.Tensor, List[torch.Tensor]]:
         _, _, h, w = x.shape
